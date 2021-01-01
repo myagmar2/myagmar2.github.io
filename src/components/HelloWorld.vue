6f2e01d3a4e95c8d73224d4fdacf3684377450b6
Khulan Myagmardorj <myagmar2@illinois.edu>
2021-01-01 17:20:58 -0500

    top section

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,120 +1,16 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-          >unit-jest</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress"
-          target="_blank"
-          rel="noopener"
-          >e2e-cypress</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="top">
+    <div class="image-block">
+      <img alt="Vue logo" src="../assets/headshot.jpeg" />
+    </div>
+    <div class="name-block">
+      <div class="first-name">Khulan</div>
+      <div class="last-name">Myagmardorj</div>
+      <div class="name-line" />
+    </div>
+    <div class="intro-block">
+      <div class="title">Hello.</div>
+    </div>
   </div>
 </template>
 
@@ -133,8 +29,64 @@ export default class HelloWorld extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.top {
+  background-color: #050505;
+  color: #fff;
+  min-height: 100vh;
+  overflow: auto;
+}
+
+.image-block {
+  z-index: 1;
+  text-align: center;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+}
+
+.image-block img {
+  height: 100vh;
+}
+
+.name-block {
+  position: absolute;
+  bottom: 3rem;
+  left: 6rem;
+  z-index: 2;
+  font-size: 5rem;
+  line-height: 5rem;
+}
+
+.name-block .first-name {
+  font-weight: bold;
+}
+
+.name-block .last-name {
+  font-family: Avenir Light, Helvetica, Arial, sans-serif;
+}
+
+.name-line {
+  border: 1px solid #fed23e;
+  border-radius: 2px;
+  margin: 1.5rem 0;
+  width: 8rem;
+}
+
+.intro-block {
+  position: absolute;
+  top: 5rem;
+  right: 6rem;
+  z-index: 2;
+}
+
+.intro-block .title {
+  font-weight: bold;
+  font-size: 1rem;
+  color: #fed23e;
+}
+
+h1 {
+  margin: 0.5rem 0 0;
 }
 ul {
   list-style-type: none;
