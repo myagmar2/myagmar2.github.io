a4d7d142e858eafec8b17e8a76f9cf63f66511e7
Khulan Myagmardorj <myagmar2@illinois.edu>
2021-01-14 00:53:15 -0500

    external links and analytics

---
+++ src/components/HomeTopSection.vue
@@ -0,0 +1,187 @@
+<template>
+  <div class="top">
+    <div class="image-block">
+      <img alt="Vue logo" src="../assets/headshot.jpeg" />
+    </div>
+    <div class="name-block">
+      <div class="first-name">Khulan</div>
+      <div class="last-name">Myagmardorj</div>
+      <div class="name-line" />
+    </div>
+    <div class="intro-block">
+      <div class="title">Hello.</div>
+      <div class="sub-title">I'm a Software Developer based in NYC.</div>
+      <div class="content">
+        At the intersection of product, design, and engineering, I’m focused on
+        front-end development and web analytics.
+      </div>
+      <div class="social">
+        <a href="https://github.com/myagmar2">
+          <svg
+            width="31"
+            height="39"
+            viewBox="0 0 31 39"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            @click="handleExternalLink"
+          >
+            <path
+              d="M27.125 6.85C27.125 4.69915 25.3871 2.95 23.25 2.95C21.1129 2.95 19.375 4.69915 19.375 6.85C19.375 8.28715 20.1597 9.53125 21.3125 10.2079V10.75C21.3125 11.8244 20.4445 12.7 19.375 12.7H14.5312C12.7391 12.7 11.0747 13.2499 9.6875 14.1859V8.2579C10.8403 7.58125 11.625 6.33715 11.625 4.9C11.625 2.74915 9.88706 1 7.75 1C5.61294 1 3.875 2.74915 3.875 4.9C3.875 6.33715 4.65969 7.58125 5.8125 8.2579V21.0421C4.65969 21.7187 3.875 22.9628 3.875 24.4C3.875 26.5508 5.61294 28.3 7.75 28.3C9.88706 28.3 11.625 26.5508 11.625 24.4C11.625 22.9804 10.8597 21.748 9.72819 21.0655C9.93937 18.5714 12.0009 16.6 14.5312 16.6H19.375C22.5796 16.6 25.1875 13.9753 25.1875 10.75V10.2079C26.3403 9.53125 27.125 8.28715 27.125 6.85ZM7.75 2.95C8.81756 2.95 9.6875 3.82555 9.6875 4.9C9.6875 5.97445 8.81756 6.85 7.75 6.85C6.68244 6.85 5.8125 5.97445 5.8125 4.9C5.8125 3.82555 6.68244 2.95 7.75 2.95ZM7.75 26.35C6.68244 26.35 5.8125 25.4764 5.8125 24.4C5.8125 23.3236 6.68244 22.45 7.75 22.45C8.81756 22.45 9.6875 23.3236 9.6875 24.4C9.6875 25.4764 8.81756 26.35 7.75 26.35ZM23.25 8.8C22.1805 8.8 21.3125 7.92445 21.3125 6.85C21.3125 5.77555 22.1805 4.9 23.25 4.9C24.3195 4.9 25.1875 5.77555 25.1875 6.85C25.1875 7.92445 24.3195 8.8 23.25 8.8Z"
+              fill="white"
+            />
+          </svg>
+        </a>
+
+        <a href="https://www.linkedin.com/in/khulan-myagmardorj/">
+          <svg
+            width="26"
+            height="26"
+            viewBox="0 0 26 26"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g clip-path="url(#clip0)">
+              <path
+                d="M5.395 4.70833C5.395 6.20442 4.1925 7.41667 2.70833 7.41667C1.22417 7.41667 0.0216667 6.20442 0.0216667 4.70833C0.0216667 3.21333 1.22417 2 2.70833 2C4.1925 2 5.395 3.21333 5.395 4.70833ZM5.41667 9.58333H0V26.9167H5.41667V9.58333ZM14.0638 9.58333H8.68183V26.9167H14.0649V17.8177C14.0649 12.7586 20.5963 12.3447 20.5963 17.8177V26.9167H26V15.9414C26 7.40475 16.3345 7.71567 14.0638 11.9179V9.58333Z"
+                fill="white"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0">
+                <rect width="26" height="26" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+        </a>
+
+        <a href="https://twitter.com/khulski?lang=en">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+              fill="white"
+            />
+          </svg>
+        </a>
+
+        <a href="/khulan-myagmardorj-resume.pdf">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              d="M4 22v-20h16v11.543c0 4.107-6 2.457-6 2.457s1.518 6-2.638 6h-7.362zm18-7.614v-14.386h-20v24h10.189c3.163 0 9.811-7.223 9.811-9.614zm-5-1.386h-10v-1h10v1zm0-4h-10v1h10v-1zm0-3h-10v1h10v-1z"
+              fill="white"
+            />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    msg: String
+  },
+  methods: {
+    handleExternalLink: () => {
+      console.log("hello");
+    }
+  }
+})
+export default class HomeTopSection extends Vue {
+  msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.top {
+  background-color: #050505;
+  color: #fff;
+  min-height: 100vh;
+  overflow: auto;
+}
+
+.image-block {
+  z-index: 1;
+  text-align: center;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+}
+
+.image-block img {
+  height: 100vh;
+}
+
+.name-block {
+  position: absolute;
+  bottom: 3rem;
+  left: 6rem;
+  z-index: 2;
+  font-size: 5rem;
+  line-height: 5rem;
+}
+
+.name-block .first-name {
+  font-weight: bold;
+}
+
+.name-block .last-name {
+  font-family: Avenir Light, Helvetica, Arial, sans-serif;
+}
+
+.name-line {
+  border: 1px solid #fed23e;
+  border-radius: 2px;
+  margin: 1.5rem 0;
+  width: 8rem;
+}
+
+.intro-block {
+  position: absolute;
+  top: 10rem;
+  right: 6rem;
+  z-index: 2;
+  max-width: 22rem;
+}
+
+.intro-block .title {
+  font-weight: bold;
+  font-size: 1rem;
+  color: #fed23e;
+}
+
+.intro-block .sub-title {
+  font-size: 1.75rem;
+}
+
+.intro-block .content {
+  font-size: 1rem;
+  color: rgb(255, 255, 255, 0.7);
+  padding: 1rem 0;
+}
+
+.intro-block .social {
+  display: flex;
+}
+
+.intro-block .social > a {
+  padding-right: 2rem;
+}
+
+.intro-block .social svg:hover path {
+  fill: #fed23e;
+}
+</style>
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,10 @@
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import HomeTopSection from "@/components/HomeTopSection.vue";
 
-describe("HelloWorld.vue", () => {
+describe("HomeTopSection.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = shallowMount(HomeTopSection, {
       props: { msg }
     });
     expect(wrapper.text()).toMatch(msg);
